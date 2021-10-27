8d97d7338c35e2f85bc5a165fa4c13b9e388b2be
Terteguh <teguhfiha@gmail.com>
2021-10-27 15:34:22 +0700

    done

--- 15-cicd/pipeline/jenkins/deploy/deploy.sh
+++ 15-cicd/pipeline/jenkins/deploy/deploy.sh
@@ -4,6 +4,6 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /opt/prod /tmp/.auth prod-user@linuxfacilito.online:/tmp/.auth
-scp -i /opt/prod ./jenkins/deploy/publish prod-user@linuxfacilito.online:/tmp/publish
-ssh -i /opt/prod prod-user@linuxfacilito.online "/tmp/publish"
+scp -i /opt/prod /tmp/.auth teguh@redhat1.mshome.net:/tmp/.auth
+scp -i /opt/prod ./jenkins/deploy/publish teguh@redhat1.mshome.net:/tmp/publish
+ssh -i /opt/prod teguh@redhat1.mshome.net "/tmp/publish"
